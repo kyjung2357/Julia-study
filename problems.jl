89a520487e7665782b8a6073a2afcb1dee71dd10
Kiyuob_Jung <kyjung2357@gmail.com>
2022-03-22 15:52:07 +0900

    20220322 - 2

--- problems.jl
+++ problems.jl
@@ -10,6 +10,21 @@ function F(n)
     end
 end
 
+# 1.1 Answer
+
+function F(n)
+    if n ≤ 2
+        return 1
+    else
+        return F(n-1) + F(n-2)
+    end
+end
+
+
 # 1.2
 
 single_line_F(n) = (n > 1 ? F(n - 1) + F(n - 2) : 1)
+
+# 2.2 Answer
+
+F1(n) = (n ≤ 2 ? 1 : F1(n-1) + F1(n-2))
